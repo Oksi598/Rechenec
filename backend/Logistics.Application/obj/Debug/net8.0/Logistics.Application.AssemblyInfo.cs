cbb7568c9477838bd1464ac94f7633c88bb29801
Oksi598 <radovunoksana@gmail.com>
2026-04-05 17:08:06 +0300

    fix

--- backend/Logistics.Application/obj/Debug/net8.0/Logistics.Application.AssemblyInfo.cs
+++ backend/Logistics.Application/obj/Debug/net8.0/Logistics.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logistics.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb125ed554ee962bdfe854f80d77417eaec64929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ff248287112709acb065f68dac221405062938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logistics.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logistics.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/Logistics/obj/Debug/net8.0/Logistics.AssemblyInfo.cs
+++ backend/Logistics/obj/Debug/net8.0/Logistics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logistics.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb125ed554ee962bdfe854f80d77417eaec64929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ff248287112709acb065f68dac221405062938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logistics.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logistics.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
