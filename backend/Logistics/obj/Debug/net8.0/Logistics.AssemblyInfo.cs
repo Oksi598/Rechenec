6ecd85c5400e4a40c53f74d1b1376972057f8b52
Oksi598 <radovunoksana@gmail.com>
2026-04-05 11:10:10 +0300

    fix

--- backend/Logistics/obj/Debug/net8.0/Logistics.AssemblyInfo.cs
+++ backend/Logistics/obj/Debug/net8.0/Logistics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logistics.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af28434b616adc437ecdac92f2e568a9d0f2fe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2efdc7bc1b265ce5c588226a8253c236f6e21e51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logistics.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logistics.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
